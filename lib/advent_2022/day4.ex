fdac7a1862e3f98b58b6a0fd7b84f41339987f05
Tim Huddle <blazhen88@gmail.com>
2022-12-04 00:20:30 -0500

    day4 done

---
+++ lib/advent_2022/day4.ex
@@ -0,0 +1,74 @@
+defmodule Advent2022.Day4 do
+  @moduledoc false
+
+  @doc ~S"""
+    iex> part1("2-4,6-8
+    ...>        2-3,4-5
+    ...>        5-7,7-9
+    ...>        2-8,3-7
+    ...>        6-6,4-6
+    ...>        2-6,4-8")
+    2
+
+    iex> Advent2022.load_data(4) |> part1()
+    515
+  """
+  @spec part1(String.t()) :: integer()
+  def part1(input) do
+    input
+    |> prepare_input()
+    |> Enum.map(&contains?/1)
+    |> Enum.filter(& &1)
+    |> Enum.count()
+  end
+
+  @doc ~S"""
+    iex> part2("2-4,6-8
+    ...>        2-3,4-5
+    ...>        5-7,7-9
+    ...>        2-8,3-7
+    ...>        6-6,4-6
+    ...>        2-6,4-8")
+    4
+
+    iex> Advent2022.load_data(4) |> part2()
+    883
+  """
+  @spec part2(String.t()) :: integer()
+  def part2(input) do
+    input
+    |> prepare_input()
+    |> Enum.map(fn [a, b] -> contains?([a, b]) or overlaps?([a, b]) end)
+    |> Enum.filter(& &1)
+    |> Enum.count()
+  end
+
+  @spec prepare_input(String.t()) :: [[Range.t()]]
+  defp prepare_input(input) do
+    input
+    |> String.trim()
+    |> String.split(~r/\n/, trim: true)
+    |> Enum.map(
+      &(&1
+        |> String.trim()
+        |> String.split(",", trim: true)
+        |> Enum.map(fn elf ->
+          elf
+          |> String.split("-", trim: true)
+          |> (fn [a, b] -> Range.new(String.to_integer(a), String.to_integer(b)) end).()
+        end))
+    )
+  end
+
+  defp contains?([range_a, range_b]) do
+    (range_a.first >= range_b.first and range_a.last <= range_b.last) or
+      (range_b.first >= range_a.first and range_b.last <= range_a.last)
+  end
+
+  defp overlaps?([range_a, range_b]) do
+    (range_a.first <= range_b.first and range_a.last >= range_b.first) or
+      (range_a.first <= range_b.last and range_a.last >= range_b.last) or
+      (range_b.first <= range_a.first and range_b.last >= range_a.first) or
+      (range_b.first <= range_a.last and range_b.last >= range_a.last)
+  end
+end
---
+++ test/advent_2022/day4_test.exs
@@ -0,0 +1,6 @@
+defmodule Advent2022.Day4Test do
+  @moduledoc false
+  use ExUnit.Case, async: true
+
+  doctest Advent2022.Day4, import: true
+end
