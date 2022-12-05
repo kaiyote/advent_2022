577b7359110c976167f784f49be3225e3b063eec
Tim Huddle <blazhen88@gmail.com>
2022-12-04 23:48:52 -0500

    stub a little more code in the generation task

--- lib/mix/tasks/gen_day.ex
+++ lib/mix/tasks/gen_day.ex
@@ -20,21 +20,28 @@ defmodule Mix.Tasks.GenDay do
       defmodule #{module_name} do
         @moduledoc false
 
-        @doc false
+        @doc ~S\"\"\"
+          iex> part1("")
+          0
+        \"\"\"
         @spec part1(String.t()) :: integer()
         def part1(input) do
-
+          0
         end
 
-        @doc false
+        @doc ~S\"\"\"
+          iex> part2("")
+          0
+        \"\"\"
         @spec part2(String.t()) :: integer()
         def part2(input) do
-
+          0
         end
 
         @spec prepare_input(String.t()) :: any()
         defp prepare_input(input) do
-
+          input
+          |> String.trim()
         end
       end
       """,
