6ba3b07d88c21f5f5603bd1f568c6c2b59a4f00f
Tim Huddle <blazhen88@gmail.com>
2022-12-10 16:14:08 -0500

    rework day 9, actually finish part 2

--- lib/advent_2022/day9.ex
+++ lib/advent_2022/day9.ex
@@ -21,19 +21,51 @@ defmodule Advent2022.Day9 do
   def part1(input) do
     input
     |> prepare_input()
-    |> Enum.reduce({[{0, 0}], [{0, 0}]}, fn move, acc -> process(move, acc) end)
-    |> elem(1)
+    |> Enum.reduce([{0, 0}], &process/2)
+    |> Enum.scan({0, 0}, &make_adjacent/2)
     |> Enum.uniq()
     |> Enum.count()
   end
 
   @doc ~S"""
-    iex> part2("")
-    0
+    iex> part2("R 4
+    ...>        U 4
+    ...>        L 3
+    ...>        D 1
+    ...>        R 4
+    ...>        D 1
+    ...>        L 5
+    ...>        R 2")
+    1
+    iex> part2("R 5
+    ...>        U 8
+    ...>        L 8
+    ...>        D 3
+    ...>        R 17
+    ...>        D 10
+    ...>        L 25
+    ...>        U 20")
+    36
+
+    iex> Advent2022.load_data(9) |> part2()
+    2566
   """
   @spec part2(String.t()) :: integer()
-  def part2(_input) do
-    0
+  def part2(input) do
+    input
+    |> prepare_input()
+    |> Enum.reduce([{0, 0}], &process/2) # H
+    |> Enum.scan({0, 0}, &make_adjacent/2) # 1
+    |> Enum.scan({0, 0}, &make_adjacent/2) # 2
+    |> Enum.scan({0, 0}, &make_adjacent/2) # 3
+    |> Enum.scan({0, 0}, &make_adjacent/2) # 4
+    |> Enum.scan({0, 0}, &make_adjacent/2) # 5
+    |> Enum.scan({0, 0}, &make_adjacent/2) # 6
+    |> Enum.scan({0, 0}, &make_adjacent/2) # 7
+    |> Enum.scan({0, 0}, &make_adjacent/2) # 8
+    |> Enum.scan({0, 0}, &make_adjacent/2) # 9
+    |> Enum.uniq()
+    |> Enum.count()
   end
 
   @spec prepare_input(String.t()) :: any()
@@ -45,44 +77,30 @@ defmodule Advent2022.Day9 do
     |> Enum.map(fn [dir, len] -> [dir, String.to_integer(len)] end)
   end
 
-  @spec process([String.t()], {[point()], [point()]}) :: {[point()], [point()]}
-  defp process(["U", len], {head, tail}),
-    do: 1..len |> Enum.reduce({head, tail}, fn _, acc -> move({0, 1}, acc) end)
+  @spec process([String.t()], [point()]) :: [point()]
+  defp process(["U", len], so_far),
+    do: 1..len |> Enum.reduce(so_far, fn _, acc -> acc ++ [add_points({0, 1}, List.last(acc))] end)
 
-  defp process(["L", len], {head, tail}),
-    do: 1..len |> Enum.reduce({head, tail}, fn _, acc -> move({-1, 0}, acc) end)
+  defp process(["D", len], so_far),
+    do: 1..len |> Enum.reduce(so_far, fn _, acc -> acc ++ [add_points({0, -1}, List.last(acc))] end)
 
-  defp process(["D", len], {head, tail}),
-    do: 1..len |> Enum.reduce({head, tail}, fn _, acc -> move({0, -1}, acc) end)
+  defp process(["L", len], so_far),
+    do: 1..len |> Enum.reduce(so_far, fn _, acc -> acc ++ [add_points({-1, 0}, List.last(acc))] end)
 
-  defp process(["R", len], {head, tail}),
-    do: 1..len |> Enum.reduce({head, tail}, fn _, acc -> move({1, 0}, acc) end)
-
-  @spec move({1 | 0 | -1, 1 | 0 | -1}, {[point()], [point()]}) :: {[point()], [point()]}
-  defp move({x, y}, {head, tail}) do
-    new_head = add_points(List.last(head), {x, y})
-    head = head ++ [new_head]
-
-    tail =
-      if adjacent?(new_head, List.last(tail)),
-        do: tail,
-        else: tail ++ [make_adjacent(new_head, List.last(tail))]
-
-    {head, tail}
-  end
+  defp process(["R", len], so_far),
+    do: 1..len |> Enum.reduce(so_far, fn _, acc -> acc ++ [add_points({1, 0}, List.last(acc))] end)
 
   @spec add_points(point(), point()) :: point()
   defp add_points({x, y}, {a, b}), do: {x + a, y + b}
 
-  @spec adjacent?(point(), point()) :: boolean()
-  defp adjacent?({x, y}, {a, b}), do: abs(x - a) <= 1 and abs(y - b) <= 1
-
   @spec make_adjacent(point(), point()) :: point()
+  defp make_adjacent({x, y}, {a, b}) when abs(x - a) <= 1 and abs(y - b) <= 1, do: {a, b}
   defp make_adjacent({x, y}, {a, b}) do
     diff_x = x - a
     diff_y = y - b
 
     cond do
+      diff_x == 0 && diff_y == 0 -> {a, b}
       diff_x == 0 -> {a, b + if(diff_y > 0, do: 1, else: -1)}
       diff_y == 0 -> {a + if(diff_x > 0, do: 1, else: -1), b}
       true -> {a + if(diff_x > 0, do: 1, else: -1), b + if(diff_y > 0, do: 1, else: -1)}
